ef653a599885a07beea81de10fa2fa7f96ea3d79
nahid <nahid.qasemi.m@gmail.com>
2026-01-01 02:33:25 +0330

    feat:add localstorage dark/light

--- src/scripts/utils/localStorage.js
+++ src/scripts/utils/localStorage.js
@@ -1,3 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //Apply saved theme on page load
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+  //Clicking on buttons (saved to LocalStorage)
+  document.addEventListener("click", (e) => {
+    // dark mode
+    if (e.target.closest(".darkBtn")) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+    // light mode
+    if (e.target.closest(".lightBtn")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  });
+});
+
 const STORAGE_KEY = "tasks"; //file name
 
 export function saveTasksToLocal(taskArray) {
